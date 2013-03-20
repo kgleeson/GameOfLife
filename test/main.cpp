7bd3f7af216fb6723b11afbf4989149f8588f30b
Kieran Gleeson <kgleeson@gmail.com>
2013-03-20 22:42:50 +0000

    Initial commit

--- test/main.cpp
+++ test/main.cpp
@@ -7,12 +7,37 @@
 //
 
 #include <iostream>
+#include <curses.h>
+#define WIDTH 80
+#define HEIGHT 25
+
+using namespace std;
+
+void setupGrid(char grid[HEIGHT][WIDTH])
+{
+    for(int i=0; i < HEIGHT ; i++){
+        for(int j=0; j< WIDTH ; j++){
+            grid[i][j] = ' ';
+        }
+    }
+}
+
+void printGrid(char grid[HEIGHT][WIDTH])
+{
+    for(int i = 0; i < HEIGHT ; i++){
+        for(int j = 0; j < WIDTH ; j++){
+            cout << grid[i][j];
+        }
+        cout << '\n';
+    }
+}
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    char grid[HEIGHT][WIDTH];
+    setupGrid(grid);
+    grid[5][5] = 'X';
+    printGrid(grid);
     return 0;
 }
 
