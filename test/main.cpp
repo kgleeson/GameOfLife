597ae3b7431b8981286c5efaa5a0da481749b6e8
Kieran Gleeson <kgleeson@gmail.com>
2013-03-20 20:53:22 +0000

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by Kieran Gleeson on 20/03/2013.
+//  Copyright (c) 2013 Kieran Gleeson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
